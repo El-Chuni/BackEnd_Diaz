82700a28f52d5b65e9d5b53db514a1958a5e7c42
El Chuni <hernandiazarro@hotmail.com>
2023-02-16 17:55:47 -0300

    Intento de handlebars

--- src/app.js
+++ src/app.js
@@ -1,14 +1,26 @@
 import express from 'express';
+import Handlebars from 'express-handlebars';
 import ProductManager from './ProductManager.js';
 import productsRouter from './routes/products.router.js';
 import cartsRouter from './routes/carts.router.js';
+import __dirname from './utils.js';
+import { Server } from 'socket.io';
+
 
 //Se hace lo necesario para activar el server
 const app = express();
 const SERVER_PORT = 8080;
+
 const server = app.listen(SERVER_PORT, () => {
   console.log(`No abran cualquier cosa en el server numero ${SERVER_PORT}.`);
 });
+
+const socketServer = new Server(server)
+
+
+app.engine('handlebars', Handlebars.engine());
+app.set('views', __dirname+'/views');
+app.set('view engine','handlebars');
 
 //Se define el manager de productos acá
 //Esto fue preparado para otra actividad y no se está usando (aún) para el proyecto final, así que ignorenlo por ahora
@@ -17,10 +29,19 @@ let managerDeProductos = new ProductManager();
 //Estos app.use nos permitirán usar routers y varias funciones sin problemas
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(express.static('public'))
+app.use(express.static(__dirname+'/public'))
+
 
 app.use('/api/products', productsRouter);
 app.use('/api/carts', cartsRouter);
+
+app.get('/', (req, res) => {
+  let testUser = {
+    name: "babe"
+  }
+
+  res.render('index', testUser);
+})
 
 app.get('/saludo', (req, res) => {
   res.send("https://www.youtube.com/watch?v=3V93ZyaiDPA");
--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,14 @@
 import multer from "multer";
+import {fileURLToPath} from 'url';
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const storage = multer.diskStorage({
     destination: function(req, file, cb){
         cb(null,'./')
     }
 })
+
+export default __dirname;
