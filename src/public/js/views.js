9029bdfbdcd402143dd975567b724940900cba42
El-Chuni <hernandiazarro@hotmail.com>
2023-02-20 19:42:58 -0300

    Intento de websocket

--- src/public/js/views.js
+++ src/public/js/views.js
@@ -1,4 +1,5 @@
 const socket = io();
+const productsContainer = document.getElementById('productsViews');
 
 socket.on('connect', () => {
     console.log('Connected to socket server');
@@ -6,7 +7,7 @@ socket.on('connect', () => {
 
 // Escuchar el evento 'updateProducts' del servidor y actualizar la vista
 socket.on('updateViews', ({ products }) => {
-  const productsContainer = document.getElementById('productsViews');
+  
   let newProductsHTML = '';
 
   products.forEach((product) => {
