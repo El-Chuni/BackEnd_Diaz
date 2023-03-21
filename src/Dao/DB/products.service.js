45e0d530a0115ee79d748d16013034a674aa8baf
El Chuni <hernandiazarro@hotmail.com>
2023-03-21 15:14:35 -0300

    Segunda pre-entrega

--- src/Dao/DB/products.service.js
+++ src/Dao/DB/products.service.js
@@ -4,40 +4,46 @@ const getProducts = async () => productModel.find();
 
 const getProductById = async (pid) => productModel.findById(pid);
 
+//Hace la paginación de los productos.
 const getProductsByParams = async (limit, page, query, sort) => {
-    const order = { limit: limit || 10, page: page || 1 };
+  const order = { limit: limit || 10, page: page || 1 };
 
-    if (sort) {
-        order.sort = sort;
-    }
+  if (sort) {
+    order.sort = sort;
+  }
 
-    let products = await productModel.paginate(query, order);
+  let products = await productModel.paginate(query, order);
 
-    return {
-        status: 'success',
-        payload: products.docs,
-        totalPages: products.totalPages,
-        prevPage: products.prevPage,
-        nextPage: products.nextPage,
-        page: products.page,
-        hasPrevPage: products.hasPrevPage,
-        hasNextPage: products.hasNextPage,
-        prevLink: products.hasPrevPage ? `?page=${products.prevPage}&limit=${limit}&query=${query}&sort=${sort}` : null,
-        nextLink: products.hasNextPage ? `?page=${products.nextPage}&limit=${limit}&query=${query}&sort=${sort}` : null,
-      };
+  return {
+    status: 'success',
+    payload: products.docs,
+    totalPages: products.totalPages,
+    prevPage: products.prevPage,
+    nextPage: products.nextPage,
+    page: products.page,
+    hasPrevPage: products.hasPrevPage,
+    hasNextPage: products.hasNextPage,
+    prevLink: products.hasPrevPage ? `?page=${products.prevPage}&limit=${limit}&query=${query}&sort=${sort}` : null,
+    nextLink: products.hasNextPage ? `?page=${products.nextPage}&limit=${limit}&query=${query}&sort=${sort}` : null,
+  };
 }
+
+const countProductsByParams = async (query) => {
+  const count = await productModel.countDocuments(query);
+  return count;
+} 
 
 const addProduct = async (body) => productModel.create(body);
 
 const updateProduct = async (pid, update) => productModel.findOneAndUpdate({id: pid}, update);
 
 const deleteProduct = async (pid) => {
-    try {
-      await productModel.deleteOne({ id: pid });
-    } catch (error) {
-      console.log(error);
-      throw new Error('Error deleting product');
-    }
+  try {
+    await productModel.deleteOne({ id: pid });
+  } catch (error) {
+    console.log(error);
+    throw new Error('Error deleting product');
+  }
 }
   
-export { getProducts, getProductById, getProductsByParams, addProduct, updateProduct, deleteProduct }
+export { getProducts, getProductById, getProductsByParams, countProductsByParams, addProduct, updateProduct, deleteProduct }
--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { getCartById } from "../Dao/DB/carts.service.js";
-import { getProducts, getProductsByParams } from "../Dao/DB/products.service.js";
+import { getProducts, getProductsByParams, countProductsByParams } from "../Dao/DB/products.service.js";
 import ProductManager from "../ProductManager.js";
 
 
@@ -10,41 +10,56 @@ const router = Router();
 
 //Esta es la vista normal que usa filesystem para un desafío previo a la segunda pre-entrega
 router.get('/', (req,res) => {
-    res.render('views',{products: productManager.getProducts()});
+  res.render('views',{products: productManager.getProducts()});
 })
 
 //Esta es para mostrar los productos con mongoDB pero extendido
 router.get('/products', async (req,res) => {
-    let limit = parseInt(req.query.limit);
-    let page = parseInt(req.query.page);
-    let query = req.query.query || ''; //Si no recibe query, el valor predeterminado es un string vacío así no clasifica nada
-    let sort = req.query.sort || null; //Si no recibe sort, el valor se vuelve nulo y no se cuenta
-    let products = await getProductsByParams(limit, page, query, sort);
+  let limit = parseInt(req.query.limit);
+  let page = parseInt(req.query.page);
+  let query = req.query.query || ''; //Si no recibe query, el valor predeterminado es un string vacío así no clasifica nada
+  let sort = req.query.sort || null; //Si no recibe sort, el valor se vuelve nulo y no se cuenta
+  let products = await getProductsByParams(limit, page, query, sort);
 
-    res.render('productsview',{products: products});
-})
+  //Calcula el número total de productos y de páginas
+  const count = await countProductsByParams(query);
+  const totalPages = Math.ceil(count / limit);
+
+  //Calcular los números de página previa y siguiente
+  const prevPage = (page > 1) ? page - 1 : null;
+  const nextPage = (page < totalPages) ? page + 1 : null;
+
+  // Renderizar la vista con la información de los productos y la paginación
+  res.render('productsview', {
+    products: products,
+    totalPages: totalPages,
+    prevPage: prevPage,
+    nextPage: nextPage
+  });
+});
+
 
 //Esto es para mostrar los productos de un carrito
 router.get('/carts/:cid', async (req, res) => {
-    try {
-      const cid = req.params.cid;
-      const cart = await getCartById(cid);
-      //Debido a un extraño error de handlebars, paso los productos de esta forma:
-      const products = cart.products.map(p => {
-        return {
-          title: p.product.title,
-          quantity: p.quantity,
-          price: p.product.price,
-          stock: p.product.stock,
-          total: (p.quantity * p.product.price)
-        };
-      });
-      
-      res.render('cart', { cid, products });
-    } catch (error) {
-      console.log(error);
-      res.status(500).send('Error interno del servidor');
-    }
+  try {
+    const cid = req.params.cid;
+    const cart = await getCartById(cid);
+    //Debido a un extraño error de handlebars, paso los productos de esta forma:
+    const products = cart.products.map(p => {
+      return {
+        title: p.product.title,
+        quantity: p.quantity,
+        price: p.product.price,
+        stock: p.product.stock,
+        total: (p.quantity * p.product.price)
+      };
+    });
+    
+    res.render('cart', { cid, products });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('Error interno del servidor');
+  }
 });
   
 
