33ad652466937063ee93a1796dd4f6b46dd64da7
El Chuni <hernandiazarro@hotmail.com>
2023-05-04 19:11:16 -0300

    Tercera pre-entrega

--- src/config/config.js
+++ src/config/config.js
@@ -22,5 +22,10 @@ export default {
     port: process.env.PORT,
     mongoUrl: process.env.MONGO_URL,
     adminName: process.env.ADMIN_NAME,
-    adminPassword: process.env.ADMIN_PASSWORD
+    adminPassword: process.env.ADMIN_PASSWORD,
+    gmailAccount: process.env.GMAIL_ACCOUNT,
+    gmailAppPassword: process.env.GMAIL_APP_PASSWD, 
+    twilioAccount: process.env.TWILIO_ACCOUNT_SID, 
+    twilioAuthToken: process.env.TWILIO_AUTH_TOKEN, 
+    twilioSMSNumber: process.env.TWILIO_SMS_NUMBER
 };
